764d26a6a86392cfb27c7803c772b33e9031a1a0
Magno <frazaomagno@gmail>
2025-07-03 17:14:42 -0300

    commit inicial

---
+++ src/test/java/com/viajante_usuario1/Usuario1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viajante_usuario1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Usuario1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
